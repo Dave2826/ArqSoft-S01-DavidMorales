2c797601ddedd34819b7a14d259e26982b8d8e5e
Dave2826 <david.morales@tecdesoftware.edu.mx>
2026-05-30 16:07:29 -0600

    Agrega perfil de usuario

---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Catalogo.Application.Services;
+using Catalogo.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo.Controllers
+{
+    public class PerfilController : Controller
+    {
+        private readonly UsuarioService _usuarioService;
+
+        public PerfilController(
+            UsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public IActionResult Index()
+        {
+            var usuarioIdString =
+                HttpContext.Session.GetString("UsuarioId");
+
+            if (string.IsNullOrEmpty(usuarioIdString))
+            {
+                return RedirectToAction(
+                    "Login",
+                    "Auth");
+            }
+
+            var usuarioId =
+                Guid.Parse(usuarioIdString);
+
+            var usuario =
+                _usuarioService.ObtenerPorId(usuarioId);
+
+            if (usuario == null)
+            {
+                return RedirectToAction(
+                    "Login",
+                    "Auth");
+            }
+
+            var model = new PerfilViewModel
+            {
+                Nombre = usuario.Nombre,
+                Apellido = usuario.Apellido,
+                Correo = usuario.Correo,
+                FechaRegistro = usuario.FechaRegistro,
+
+                TotalMotocicletas = 0,
+                ServiciosRealizados = 0,
+                GastoAcumulado = 0
+            };
+
+            return View(model);
+        }
+    }
+}
